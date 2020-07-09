2792fc5499ca08bee784377a69ea78d3b8f4e217
Magicono43 <captmagik@hotmail.com>
2020-07-09 14:07:53 -0400

    Temporary Additions For Item Mod, Ignore and Remove Eventually
    Temporary Additions For Item Mod, Ignore and Remove Eventually

--- PhysicalCombatAndArmorOverhaul.cs
+++ PhysicalCombatAndArmorOverhaul.cs
@@ -35,14 +35,64 @@ namespace PhysicalCombatAndArmorOverhaul
 		public static bool armorHitFormulaModuleCheck { get; set; }
 		public static bool shieldBlockSuccess { get; set; }
 
+        /////////////////////////////////////////////////////////////////////
+        public static GameObject ExampleGo;
+        public static Mod myMod;
+        public static AudioSource audioSource;
+
+        //public const string audioClips = "Zero Gravity 1.ogg";
+
+        //list of audio clip assets bundled in mod
+        public static readonly List<string> audioClips = new List<string>()
+        {
+            "Blade Sharpen WhetStone 1.mp3",
+            "Sewing Kit Repair 1.mp3",
+            "Armorers Hammer Repair 1.mp3",
+            "Jewelers Pliers Repair 1.mp3",
+            "Epoxy Glue Repair 1.mp3"
+        };
+
+        void Start()
+        {
+            //get reference to mod object.  
+            myMod = mod;
+
+            //Can also get this using ModManager, using modtitle or index
+            // myMod = ModManager.Instance.GetMod(modtitle);
+
+            if (audioSource == null)
+                audioSource = this.GetComponent<AudioSource>();
+        }
+        /////////////////////////////////////////////////////////////////////
+
         [Invoke(StateManager.StateTypes.Start, 0)]
         public static void Init(InitParams initParams)
         {
             mod = initParams.Mod;
             var go = new GameObject("PhysicalCombatAndArmorOverhaul");
             go.AddComponent<PhysicalCombatAndArmorOverhaul>();
-        }
 
+            ItemHelper itemHelper = DaggerfallUnity.Instance.ItemHelper;
+
+            itemHelper.RegisterCustomItem(ItemWhetstone.templateIndex, ItemGroups.UselessItems2, typeof(ItemWhetstone));
+            itemHelper.RegisterCustomItem(ItemSewingKit.templateIndex, ItemGroups.UselessItems2, typeof(ItemSewingKit));
+            itemHelper.RegisterCustomItem(ItemArmorersHammer.templateIndex, ItemGroups.UselessItems2, typeof(ItemArmorersHammer));
+            itemHelper.RegisterCustomItem(ItemJewelersPliers.templateIndex, ItemGroups.UselessItems2, typeof(ItemJewelersPliers));
+            itemHelper.RegisterCustomItem(ItemEpoxyGlue.templateIndex, ItemGroups.UselessItems2, typeof(ItemEpoxyGlue));
+        }
+			/////////////////////////////////////////////////////////////////////
+        [Invoke(StateManager.StateTypes.Game)]
+        public static void InitAtGameState(InitParams initParams)
+        {
+            if (ExampleGo != null)
+                return;
+            //Get a clone of the example gameobject prefab from mod
+            ExampleGo = mod.GetAsset<GameObject>("Example.prefab", true);
+
+            //add the audio player script to it
+            ExampleGo.AddComponent<PhysicalCombatAndArmorOverhaul>();
+        }
+            /////////////////////////////////////////////////////////////////////
         void Awake()
         {	
             ModSettings settings = mod.GetSettings();
