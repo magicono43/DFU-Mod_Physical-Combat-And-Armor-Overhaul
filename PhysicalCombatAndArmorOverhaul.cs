14df2774e0ac4396879f6a53776bad0ed243082a
Magicono43 <captmagik@hotmail.com>
2020-07-24 21:39:23 -0400

    Updated Files for v1.20 Release, 7-24-2020, 9:40 PM
    Updated Files for v1.20 Release, 7-24-2020, 9:40 PM

--- PhysicalCombatAndArmorOverhaul.cs
+++ PhysicalCombatAndArmorOverhaul.cs
@@ -23,7 +23,6 @@ using DaggerfallWorkshop.Game.MagicAndEffects;
 using DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects;
 using UnityEngine;
 using System;
-using System.Collections.Generic;
 
 namespace PhysicalCombatAndArmorOverhaul
 {
@@ -37,64 +36,14 @@ namespace PhysicalCombatAndArmorOverhaul
         public static bool condBasedEffectModuleCheck { get; set; }
         public static bool shieldBlockSuccess { get; set; }
 
-        /////////////////////////////////////////////////////////////////////
-        public static GameObject ExampleGo;
-        public static Mod myMod;
-        public static AudioSource audioSource;
-
-        //public const string audioClips = "Zero Gravity 1.ogg";
-
-        //list of audio clip assets bundled in mod
-        public static readonly List<string> audioClips = new List<string>()
-        {
-            "Blade Sharpen WhetStone 1.mp3",
-            "Sewing Kit Repair 1.mp3",
-            "Armorers Hammer Repair 1.mp3",
-            "Jewelers Pliers Repair 1.mp3",
-            "Epoxy Glue Repair 1.mp3"
-        };
-
-        void Start()
-        {
-            //get reference to mod object.  
-            myMod = mod;
-
-            //Can also get this using ModManager, using modtitle or index
-            // myMod = ModManager.Instance.GetMod(modtitle);
-
-            if (audioSource == null)
-                audioSource = this.GetComponent<AudioSource>();
-        }
-        /////////////////////////////////////////////////////////////////////
-
         [Invoke(StateManager.StateTypes.Start, 0)]
         public static void Init(InitParams initParams)
         {
             mod = initParams.Mod;
             var go = new GameObject("PhysicalCombatAndArmorOverhaul");
             go.AddComponent<PhysicalCombatAndArmorOverhaul>();
-
-            ItemHelper itemHelper = DaggerfallUnity.Instance.ItemHelper;
-
-            itemHelper.RegisterCustomItem(ItemWhetstone.templateIndex, ItemGroups.UselessItems2, typeof(ItemWhetstone));
-            itemHelper.RegisterCustomItem(ItemSewingKit.templateIndex, ItemGroups.UselessItems2, typeof(ItemSewingKit));
-            itemHelper.RegisterCustomItem(ItemArmorersHammer.templateIndex, ItemGroups.UselessItems2, typeof(ItemArmorersHammer));
-            itemHelper.RegisterCustomItem(ItemJewelersPliers.templateIndex, ItemGroups.UselessItems2, typeof(ItemJewelersPliers));
-            itemHelper.RegisterCustomItem(ItemEpoxyGlue.templateIndex, ItemGroups.UselessItems2, typeof(ItemEpoxyGlue));
         }
-			/////////////////////////////////////////////////////////////////////
-        [Invoke(StateManager.StateTypes.Game)]
-        public static void InitAtGameState(InitParams initParams)
-        {
-            if (ExampleGo != null)
-                return;
-            //Get a clone of the example gameobject prefab from mod
-            ExampleGo = mod.GetAsset<GameObject>("Example.prefab", true);
 
-            //add the audio player script to it
-            ExampleGo.AddComponent<PhysicalCombatAndArmorOverhaul>();
-        }
-            /////////////////////////////////////////////////////////////////////
         void Awake()
         {	
             ModSettings settings = mod.GetSettings();
@@ -2586,7 +2535,7 @@ namespace PhysicalCombatAndArmorOverhaul
             string majorDamageItemMessage = "";
             int condDiff = startItemCondPer - item.ConditionPercentage;
 			
-			if (item.ConditionPercentage <= 49 || condDiff >= 12)
+			if (item.ConditionPercentage <= 49 || condDiff >= 15)
 			{
                 if (item.IsEnchanted) // All Magically Enchanted Items Text
                 {
@@ -2663,7 +2612,7 @@ namespace PhysicalCombatAndArmorOverhaul
 					DaggerfallUI.AddHUDText(roughItemMessage, 2.00f); // Possibly make a random between a few of these lines to mix it up or something.				
 				else if (item.ConditionPercentage == 15) // 16 & 12
 					DaggerfallUI.AddHUDText(damagedItemMessage, 2.00f);
-                else if (condDiff >= 12)
+                else if (condDiff >= 15)
                     DaggerfallUI.AddHUDText(majorDamageItemMessage, 2.00f);
             }
 		}
