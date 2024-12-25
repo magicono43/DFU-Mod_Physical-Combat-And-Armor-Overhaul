7b11b8357c5e6aa0d84640067d931ee1a5f4fc6b
magicono43 <kirkoliveri@gmail.com>
2024-12-24 21:10:50 -0500

    Added Barebones Framework For New PCO Info Window, 12/24/2024.
    Added Barebones Framework For New PCO Info Window, 12/24/2024.

---
+++ Scripts/PCOInfoWindow.cs
@@ -0,0 +1,198 @@
+using UnityEngine;
+using DaggerfallWorkshop.Utility;
+using DaggerfallWorkshop.Game.UserInterface;
+using PhysicalCombatOverhaul;
+using DaggerfallWorkshop.Game.Entity;
+using DaggerfallWorkshop.Game.Items;
+using DaggerfallWorkshop.Game.Serialization;
+
+namespace DaggerfallWorkshop.Game.UserInterfaceWindows
+{
+    /// <summary>
+    /// Implements Physical Combat Overhaul's Custom Info Window.
+    /// </summary>
+    public class PCOInfoWindow : DaggerfallPopupWindow
+    {
+        PlayerEntity player;
+
+        PlayerEntity Player
+        {
+            get { return (player != null) ? player : player = GameManager.Instance.PlayerEntity; }
+        }
+
+        #region Testing Properties
+
+        public static Rect butt1 = new Rect(0, 0, 0, 0);
+        public static Rect butt2 = new Rect(0, 0, 0, 0);
+        public static Rect butt3 = new Rect(0, 0, 0, 0);
+
+        #endregion
+
+        #region Constructors
+
+        public PCOInfoWindow(IUserInterfaceManager uiManager)
+            : base(uiManager)
+        {
+        }
+
+        #endregion
+
+        #region UI Textures
+
+        Texture2D baseTexture;
+
+        #endregion
+
+        protected override void Setup()
+        {
+            base.Setup();
+
+            // Load textures
+            LoadTextures();
+
+            // This makes the background "transparent" instead of a blank black screen when opening this window.
+            ParentPanel.BackgroundColor = ScreenDimColor;
+
+            // Setup native panel background
+            NativePanel.BackgroundColor = ScreenDimColor;
+            NativePanel.BackgroundTexture = baseTexture;
+
+            SetupChestChoiceButtons();
+        }
+
+        protected virtual void LoadTextures()
+        {
+            //baseTexture = LockedLootContainersMain.Instance.ChestChoiceMenuTexture;
+        }
+
+        protected void SetupChestChoiceButtons()
+        {
+            // Inspect Chest button
+            Button inspectChestButton = DaggerfallUI.AddButton(new Rect(144, 70, 33, 16), NativePanel);
+            inspectChestButton.ToolTip = defaultToolTip;
+            inspectChestButton.ToolTipText = "Inspect Chest";
+            //inspectChestButton.OnMouseClick += InspectChestButton_OnMouseClick;
+            inspectChestButton.ClickSound = DaggerfallUI.Instance.GetAudioClip(SoundClips.ButtonClick);
+
+            // Attempt Lockpick button
+            Button attemptLockpickButton = DaggerfallUI.AddButton(new Rect(144, 92, 33, 16), NativePanel);
+            attemptLockpickButton.ToolTip = defaultToolTip;
+            attemptLockpickButton.ToolTipText = "Attempt Lockpick";
+            //attemptLockpickButton.OnMouseClick += AttemptLockpickButton_OnMouseClick;
+            attemptLockpickButton.ClickSound = DaggerfallUI.Instance.GetAudioClip(SoundClips.ButtonClick);
+
+            // Exit button
+            Button exitButton = DaggerfallUI.AddButton(new Rect(142, 114, 36, 17), NativePanel);
+            exitButton.OnMouseClick += ExitButton_OnMouseClick;
+            exitButton.ClickSound = DaggerfallUI.Instance.GetAudioClip(SoundClips.ButtonClick);
+        }
+
+        /*
+        private void InspectChestButton_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            CloseWindow();
+            if (chest.HasBeenInspected) { } // Do nothing, will likely change this eventually, so reinspection/rerolling for inspection results is possible at some cost or something.
+            else
+            {
+                LockedLootContainersMain.ApplyInspectionCosts();
+                chest.RecentInspectValues = LockedLootContainersMain.GetInspectionValues(chest);
+                chest.HasBeenInspected = true;
+            }
+            InspectionInfoWindow inspectionInfoWindow = new InspectionInfoWindow(DaggerfallUI.UIManager, chest);
+            DaggerfallUI.UIManager.PushWindow(inspectionInfoWindow);
+        }
+
+        private void AttemptLockpickButton_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            CloseWindow();
+            if (chest != null)
+            {
+                DaggerfallAudioSource dfAudioSource = chest.GetComponent<DaggerfallAudioSource>();
+                ItemCollection closedChestLoot = chest.AttachedLoot;
+                Transform closedChestTransform = chest.transform;
+                Vector3 pos = chest.transform.position;
+
+                LockedLootContainersMain.IsThisACrime(ChestInteractionType.Lockpick);
+
+                if (chest.IsLockJammed)
+                {
+                    DaggerfallUI.AddHUDText(LockedLootContainersMain.GetLockAlreadyJammedText(), 2f);
+                    if (dfAudioSource != null && !dfAudioSource.IsPlaying())
+                        dfAudioSource.AudioSource.PlayOneShot(LockedLootContainersMain.GetLockAlreadyJammedClip(), UnityEngine.Random.Range(0.9f, 1.42f) * DaggerfallUnity.Settings.SoundVolume);
+                }
+                else if (LockedLootContainersMain.LockPickChance(chest))
+                {
+                    chest.PicksAttempted++;
+                    LockedLootContainersMain.ApplyLockPickAttemptCosts();
+
+                    DaggerfallLoot openChestLoot = null;
+                    if (LockedLootContainersMain.ChestGraphicType == 0) // Use sprite based graphics for chests
+                    {
+                        int spriteID = closedChestLoot.Count <= 0 ? LockedLootContainersMain.OpenEmptyChestSpriteID : LockedLootContainersMain.OpenFullChestSpriteID;
+                        openChestLoot = GameObjectHelper.CreateLootContainer(LootContainerTypes.Nothing, InventoryContainerImages.Chest, pos, closedChestTransform.parent, spriteID, 0, DaggerfallUnity.NextUID, null, false);
+                        openChestLoot.gameObject.name = GameObjectHelper.GetGoFlatName(spriteID, 0);
+                        openChestLoot.Items.TransferAll(closedChestLoot); // Transfers items from closed chest's items to the new open chest's item collection.
+                        GameObject.Destroy(openChestLoot.GetComponent<SerializableLootContainer>());
+                    }
+                    else // Use 3D models for chests
+                    {
+                        GameObject usedModelPrefab = null;
+                        int modelID = 0;
+                        if (closedChestLoot.Count <= 0) { usedModelPrefab = (LockedLootContainersMain.ChestGraphicType == 1) ? LockedLootContainersMain.Instance.LowPolyOpenEmptyChestPrefab : LockedLootContainersMain.Instance.HighPolyOpenEmptyChestPrefab; modelID = LockedLootContainersMain.OpenEmptyChestModelID; }
+                        else { usedModelPrefab = (LockedLootContainersMain.ChestGraphicType == 1) ? LockedLootContainersMain.Instance.LowPolyOpenFullChestPrefab : LockedLootContainersMain.Instance.HighPolyOpenFullChestPrefab; modelID = LockedLootContainersMain.OpenFullChestModelID; }
+                        GameObject chestGo = GameObjectHelper.InstantiatePrefab(usedModelPrefab, GameObjectHelper.GetGoModelName((uint)modelID), closedChestTransform.parent, pos);
+                        chestGo.transform.rotation = chest.gameObject.transform.rotation;
+                        Collider col = chestGo.AddComponent<BoxCollider>();
+                        openChestLoot = chestGo.AddComponent<DaggerfallLoot>();
+                        LockedLootContainersMain.ToggleChestShadowsOrCollision(chestGo);
+                        if (openChestLoot)
+                        {
+                            openChestLoot.ContainerType = LootContainerTypes.Nothing;
+                            openChestLoot.ContainerImage = InventoryContainerImages.Chest;
+                            openChestLoot.LoadID = DaggerfallUnity.NextUID;
+                            openChestLoot.TextureRecord = modelID;
+                            openChestLoot.Items.TransferAll(closedChestLoot); // Transfers items from closed chest's items to the new open chest's item collection.
+                        }
+                    }
+
+                    // Show success and play unlock sound
+                    DaggerfallUI.AddHUDText(LockedLootContainersMain.GetLockPickSuccessText(), 3f);
+                    if (dfAudioSource != null)
+                        AudioSource.PlayClipAtPoint(LockedLootContainersMain.GetLockpickSuccessClip(), chest.gameObject.transform.position, UnityEngine.Random.Range(1.5f, 2.31f) * DaggerfallUnity.Settings.SoundVolume);
+
+                    UnityEngine.Object.Destroy(LockedLootContainersMain.ChestObjRef); // Remove closed chest from scene.
+                    LockedLootContainersMain.ChestObjRef = null;
+                }
+                else
+                {
+                    chest.PicksAttempted++; // Increase picks attempted counter by 1 on the chest.
+                    LockedLootContainersMain.ApplyLockPickAttemptCosts();
+                    int mechDamDealt = LockedLootContainersMain.DetermineDamageToLockMechanism(chest);
+
+                    if (LockedLootContainersMain.DoesLockJam(chest, mechDamDealt))
+                    {
+                        DaggerfallUI.AddHUDText(LockedLootContainersMain.GetJammedLockText(), 3f);
+                        if (dfAudioSource != null)
+                            AudioSource.PlayClipAtPoint(LockedLootContainersMain.GetLockpickJammedClip(), chest.gameObject.transform.position, UnityEngine.Random.Range(8.2f, 9.71f) * DaggerfallUnity.Settings.SoundVolume);
+                    }
+                    else
+                    {
+                        DaggerfallUI.AddHUDText(LockedLootContainersMain.GetLockPickAttemptText(), 2f);
+                        if (dfAudioSource != null && !dfAudioSource.IsPlaying())
+                            dfAudioSource.AudioSource.PlayOneShot(LockedLootContainersMain.GetLockpickAttemptClip(), UnityEngine.Random.Range(1.2f, 1.91f) * DaggerfallUnity.Settings.SoundVolume);
+                    }
+                }
+            }
+            else
+            {
+                DaggerfallUI.AddHUDText("ERROR: Chest Was Found As Null.", 3f);
+            }
+        }
+        */
+
+        private void ExitButton_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            CloseWindow();
+        }
+    }
+}
